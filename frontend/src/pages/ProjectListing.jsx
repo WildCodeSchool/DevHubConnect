cab5eedda18513b43b0e33512528c8e60091b4c7
Binou05 <sabine.arnaud-bertoneri@sncf.fr>
2023-02-23 15:33:14 +0100

    arborescence projectListing et ProjectSingle

--- frontend/src/pages/ProjectListing.jsx
+++ frontend/src/pages/ProjectListing.jsx
@@ -1,10 +1,21 @@
+/* eslint-disable import/newline-after-import */
+/* eslint-disable import/no-duplicates */
+/* eslint-disable prettier/prettier */
+// eslint-disable-next-line import/extensions, import/no-unresolved, spaced-comment
+// eslint-disable-next-line import/order
 import React from "react";
 
+// import ProjectListingContainer from "../components/Project/ProjectListing/ProjectContainer/ProjectListingContainer";
+// eslint-disable-next-line import/no-duplicates
+import ProjectListingContainer from "../components/Project/ProjectListing/ProjectContainer/ProjectListingContainer";
 function ProjectListing() {
   return (
     <>
-      <h1>ProjectListing</h1>
-      <p>ajouter les cartes en flex</p>
+      {/* <ProjectListingHeader /> */}
+      {/* <ProjectListingCard /> */}
+      {/* <ProjectListingCard /> */}
+      {/* <ProjectListingCard /> */}
+      <ProjectListingContainer />
     </>
   );
 }
