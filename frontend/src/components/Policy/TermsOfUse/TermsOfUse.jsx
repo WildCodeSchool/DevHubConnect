ef299af74b6d680485eac551794f0de65d3bf049
jejem8430 <jer.munoz30@gmail.com>
2023-02-24 13:36:41 +0100

    create cgu

---
+++ frontend/src/components/Policy/TermsOfUse/TermsOfUse.jsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+
+export default function TermsOfUse() {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Typography variant="h5" gutterBottom>
+        1.Préambule
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        Les présentes conditions générales (ci-après les "Conditions Générales")
+        régissent les relations entre HUB Connect (ci-après "HUB Connect" ou
+        "nous") et les utilisateurs (ci-après "utilisateur" ou "vous") de notre
+        plateforme en ligne qui met en relation des développeurs et des PO au
+        sein de la SNCF pour organiser des projets web .
+      </Typography>
+
+      <Typography variant="h5" gutterBottom>
+        2. Inscription et utilisation de la plateforme
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        2.1. Pour pouvoir utiliser la plateforme, vous devez vous inscrire en
+        créant un compte en remplissant le formulaire d'inscription.
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        2.2. L'inscription est gratuite.{" "}
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        2.3. Vous devez fournir des informations exactes, complètes et à jour
+        lors de votre inscription.{" "}
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        2.4. Vous devez maintenir la confidentialité de votre mot de passe et
+        être responsable de toutes les activités qui ont lieu sous votre compte.
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        2.5. Vous devez signaler immédiatement tout usage non autorisé de votre
+        compte ou toute violation de sécurité à HUB Connect.{" "}
+      </Typography>
+
+      <Typography variant="body2" gutterBottom>
+        2.6. Vous devez respecter toutes les lois et règlements applicables lors
+        de l'utilisation de la plateforme.
+      </Typography>
+
+      <Typography variant="h5" gutterBottom>
+        3. Contenu
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        3.1. La plateforme peut contenir des textes, images, vidéos,
+        commentaires, réactions et autres contenus (ci-après le "Contenu").
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        3.2.Vous êtes entièrement responsable de tout Contenu que vous publiez
+        sur la plateforme.
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        3.3. Vous garantissez que vous avez tous les droits et autorisations
+        nécessaires pour publier tout Contenu sur la plateforme.
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        3.4. Vous devez respecter les droits de propriété intellectuelle de
+        tiers lors de la publication de Contenu sur la plateforme.
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        3.5. Vous ne devez pas publier de Contenu qui est illégal, diffamatoire,
+        injurieux, raciste, discriminatoire, offensant, vulgaire, violent,
+        incitant à la violence ou qui porte atteinte à la vie privée de tiers.
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        3.6. HUB Connect se réserve le droit de supprimer tout Contenu qui
+        enfreint les Conditions Générales.
+      </Typography>
+
+      <Typography variant="h5" gutterBottom>
+        4. Responsabilité
+      </Typography>
+
+      <Typography variant="body1" gutterBottom>
+        4.1. Vous êtes entièrement responsable de tout dommage causé par votre
+        utilisation de la plateforme.
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        4.2. HUB Connect ne peut pas être tenu responsable de tout dommage causé
+        par l'utilisation de la plateforme, y compris les dommages indirects,
+        accessoires, consécutifs, exemplaires .
+      </Typography>
+    </Box>
+  );
+}
--- frontend/src/pages/ProjectListing.jsx
+++ frontend/src/pages/ProjectListing.jsx
@@ -1,23 +1,8 @@
-/* eslint-disable import/newline-after-import */
-/* eslint-disable import/no-duplicates */
-/* eslint-disable prettier/prettier */
-// eslint-disable-next-line import/extensions, import/no-unresolved, spaced-comment
-// eslint-disable-next-line import/order
+
 import React from "react";
 
-// import ProjectListingContainer from "../components/Project/ProjectListing/ProjectContainer/ProjectListingContainer";
-// eslint-disable-next-line import/no-duplicates
-import ProjectListingContainer from "../components/Project/ProjectListing/ProjectContainer/ProjectListingContainer";
 function ProjectListing() {
-  return (
-    <>
-      {/* <ProjectListingHeader /> */}
-      {/* <ProjectListingCard /> */}
-      {/* <ProjectListingCard /> */}
-      {/* <ProjectListingCard /> */}
-      <ProjectListingContainer />
-    </>
-  );
+  return <h1>project listinf</h1>;
 }
 
 export default ProjectListing;
