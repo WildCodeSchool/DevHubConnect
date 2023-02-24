8d0884264785743f5793892578b09377fb1058a2
jejem8430 <jer.munoz30@gmail.com>
2023-02-24 19:42:31 +0100

    create cards header accordion

--- frontend/src/components/User/UserProject/UserProjectCurrentProject/UserProjectCurrentProject.jsx
+++ frontend/src/components/User/UserProject/UserProjectCurrentProject/UserProjectCurrentProject.jsx
@@ -1,7 +1,159 @@
 import React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
 
-function UserProjectCurrentProject() {
-  return <div>UserProjectCurrentProject</div>;
+export default function UserProjectCurrentProject() {
+  return (
+    <div>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Projets en cours</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Retrouvez ici vos projets en cours.</Typography>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="150"
+              image="https://picsum.photos/500/300"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2a-content"
+          id="panel2a-header"
+        >
+          <Typography>Projets à venir</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="https://picsum.photos/500/300"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Projets non retenus</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="https://picsum.photos/500/300"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3a-content"
+          id="panel3a-header"
+        >
+          <Typography>Tous les projets</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Retrouvez ici l'ensemble de vos projets.</Typography>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              alt="green iguana"
+              height="140"
+              image="https://picsum.photos/500/300"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                Lizard
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lizards are a widespread group of squamate reptiles, with over
+                6,000 species, ranging across all continents except Antarctica
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Share</Button>
+              <Button size="small">Learn More</Button>
+            </CardActions>
+          </Card>
+        </AccordionDetails>
+      </Accordion>
+    </div>
+  );
 }
-
-export default UserProjectCurrentProject;
--- frontend/src/components/User/UserProject/UserProjectHeader/UserProjectHeader.jsx
+++ frontend/src/components/User/UserProject/UserProjectHeader/UserProjectHeader.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import { Chip } from "@mui/material";
 
 function UserProjectHeader() {
-  return <div>UserProjectHeader</div>;
+  return (
+    <>
+      <Typography gutterBottom variant="h1" component="div">
+        UserProject
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        <Stack direction="row" spacing={4} p={2}>
+          <Chip label="Projets en cours" size="medium" color="primary" />
+          <Chip label="Projets à venir" size="medium" color="primary" />
+          <Chip label="Projets non retenus" size="medium" color="primary" />
+          <Chip label="Tous les projets" size="medium" color="primary" />
+        </Stack>
+      </Typography>
+    </>
+  );
 }
-
 export default UserProjectHeader;
