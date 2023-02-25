8b4963dd2169807a9f9cb42a58c0094113aa1ed4
jejem8430 <jer.munoz30@gmail.com>
2023-02-25 17:42:38 +0100

    update avatar cards

--- frontend/src/components/User/UserProject/UserProjectCurrentProject/UserProjectCurrentProject.jsx
+++ frontend/src/components/User/UserProject/UserProjectCurrentProject/UserProjectCurrentProject.jsx
@@ -10,6 +10,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Stack from "@mui/material/Stack";
+import Avatar from "@mui/material/Avatar";
 
 export default function UserProjectCurrentProject() {
   return (
@@ -42,10 +43,15 @@ export default function UserProjectCurrentProject() {
                     over 6,000 species, ranging across all continents except
                     Antarctica
                   </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
                 </CardActions>
               </Card>
             </div>
@@ -67,10 +73,15 @@ export default function UserProjectCurrentProject() {
                     over 6,000 species, ranging across all continents except
                     Antarctica
                   </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
                 </CardActions>
               </Card>
             </div>
@@ -92,10 +103,118 @@ export default function UserProjectCurrentProject() {
                     over 6,000 species, ranging across all continents except
                     Antarctica
                   </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
                 </CardContent>
                 <CardActions>
-                  <Button size="small">Share</Button>
-                  <Button size="small">Learn More</Button>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Stack>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>Projets à venir</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Stack direction="row" spacing={2} p={1}>
+            <div>
+              <Typography>Projet N°235 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Application web
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
+                </CardActions>
+              </Card>
+            </div>
+            <div>
+              <Typography>Projet N°236 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Boutique en ligne
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
+                </CardActions>
+              </Card>
+            </div>
+            <div>
+              <Typography>Projet N°237 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Creation BDD
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
                 </CardActions>
               </Card>
             </div>
@@ -106,74 +225,104 @@ export default function UserProjectCurrentProject() {
       <Accordion>
         <AccordionSummary
           expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel2a-content"
-          id="panel2a-header"
-        >
-          <Typography>Projets à venir</Typography>
-        </AccordionSummary>
-        <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://picsum.photos/500/300"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </AccordionDetails>
-      </Accordion>
-
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
+          aria-controls="panel1a-content"
+          id="panel1a-header"
         >
           <Typography>Projets non retenus</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-            malesuada lacus ex, sit amet blandit leo lobortis eget.
-          </Typography>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image="https://picsum.photos/500/300"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <Stack direction="row" spacing={2} p={1}>
+            <div>
+              <Typography>Projet N°235 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Application web
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
+                </CardActions>
+              </Card>
+            </div>
+            <div>
+              <Typography>Projet N°236 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Boutique en ligne
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
+                </CardActions>
+              </Card>
+            </div>
+            <div>
+              <Typography>Projet N°237 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Creation BDD
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                  <Avatar
+                    alt="Remy Sharp"
+                    src="https://xsgames.co/randomusers/avatar.php?g=male"
+                    sx={{ width: 100, height: 100, ml: 10, mt: 2 }}
+                  />
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Partager</Button>
+                  <Button size="small">+ d'infos</Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Stack>
         </AccordionDetails>
       </Accordion>
 
