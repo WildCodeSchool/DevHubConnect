71fbbbd3b8b8e5993aab808588fb3d396ea8a5aa
jejem8430 <jer.munoz30@gmail.com>
2023-02-25 12:36:33 +0100

    update current project

--- frontend/src/components/User/UserProject/UserProjectCurrentProject/UserProjectCurrentProject.jsx
+++ frontend/src/components/User/UserProject/UserProjectCurrentProject/UserProjectCurrentProject.jsx
@@ -9,6 +9,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
 
 export default function UserProjectCurrentProject() {
   return (
@@ -22,28 +23,83 @@ export default function UserProjectCurrentProject() {
           <Typography>Projets en cours</Typography>
         </AccordionSummary>
         <AccordionDetails>
-          <Typography>Retrouvez ici vos projets en cours.</Typography>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              alt="green iguana"
-              height="150"
-              image="https://picsum.photos/500/300"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Lizards are a widespread group of squamate reptiles, with over
-                6,000 species, ranging across all continents except Antarctica
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
+          <Stack direction="row" spacing={2} p={1}>
+            <div>
+              <Typography>Projet N°235 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Application web
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </div>
+            <div>
+              <Typography>Projet N°236 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Boutique en ligne
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </div>
+            <div>
+              <Typography>Projet N°237 .</Typography>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  alt="green iguana"
+                  height="150"
+                  image="https://picsum.photos/500/300"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Projet Creation BDD
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Lizards are a widespread group of squamate reptiles, with
+                    over 6,000 species, ranging across all continents except
+                    Antarctica
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small">Share</Button>
+                  <Button size="small">Learn More</Button>
+                </CardActions>
+              </Card>
+            </div>
+          </Stack>
         </AccordionDetails>
       </Accordion>
 
