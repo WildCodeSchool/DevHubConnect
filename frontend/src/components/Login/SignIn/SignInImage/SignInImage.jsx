221f70599d54c44a10c566291ef4f682896aae7f
fredl2tp <fleschira@gmail.com>
2023-02-27 08:07:46 +0100

    begin feat signin

---
+++ frontend/src/components/Login/SignIn/SignInImage/SignInImage.jsx
@@ -0,0 +1,15 @@
+// import React from "react";
+
+// function SignInImage() {
+//   return (
+//     <div>
+//       <Image
+//         src="https://i.ytimg.com/vi/yaqe1qesQ8c/maxresdefault.jpg"
+//         aspectRatio={16 / 9}
+//         disableSpinner
+//       />
+//     </div>
+//   );
+// }
+
+// export default SignInImage;
