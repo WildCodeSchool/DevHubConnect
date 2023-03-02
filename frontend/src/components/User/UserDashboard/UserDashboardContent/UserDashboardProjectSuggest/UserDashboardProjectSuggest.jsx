632cf4ea64e76b387cb304877fcc8d3337bb70e3
fredericgalline <frederic@galline.fr>
2023-03-02 17:28:12 +0100

    Pull API skills and projectSkill => OK

--- frontend/src/components/User/UserDashboard/UserDashboardContent/UserDashboardProjectSuggest/UserDashboardProjectSuggest.jsx
+++ frontend/src/components/User/UserDashboard/UserDashboardContent/UserDashboardProjectSuggest/UserDashboardProjectSuggest.jsx
@@ -5,11 +5,15 @@ import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import Chip from "@mui/material/Chip";
 import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
 
 // Définition du composant fonctionnel UserDashboardProjectSuggest
 export default function UserDashboardProjectSuggest() {
   // Utilisation de useState pour créer un state projectListing et de la fonction setProjectListing pour modifier ce state
   const [projectListing, setProjectListing] = useState([]);
+  const [projectSkillListing, setProjectSkillListing] = useState([]);
+  const [skillListing, setSkillListing] = useState([]);
 
   // Définition de la fonction getProjects pour récupérer les projets à partir de l'API REST
   const getProjects = () => {
@@ -26,9 +30,38 @@ export default function UserDashboardProjectSuggest() {
       });
   };
 
-  // Utilisation de useEffect pour exécuter la fonction getProjects lors du premier rendu du composant
+  const getProjectSkill = () => {
+    axios
+      .get("http://localhost:5006/project_skills", {
+        headers: {
+          "Access-Control-Allow-Origin": "http://localhost:3000", // L'URL de votre front-end
+        },
+      })
+      .then((response) => response.data)
+      .then((projectsSkillData) => {
+        // Utilisation de setProjectListing pour mettre à jour le state projectListing avec les données de l'API
+        setProjectSkillListing(projectsSkillData);
+      });
+  };
+
+  const getSkill = () => {
+    axios
+      .get("http://localhost:5006/skills", {
+        headers: {
+          "Access-Control-Allow-Origin": "http://localhost:3000", // L'URL de votre front-end
+        },
+      })
+      .then((response) => response.data)
+      .then((skillData) => {
+        // Utilisation de setProjectListing pour mettre à jour le state projectListing avec les données de l'API
+        setSkillListing(skillData);
+      });
+  };
+
   useEffect(() => {
     getProjects();
+    getSkill();
+    getProjectSkill();
   }, []);
 
   const formatDate = (dateString) => {
@@ -42,75 +75,99 @@ export default function UserDashboardProjectSuggest() {
     <>
       {projectListing.slice(-5).map((project) => {
         return (
-          <Paper
-            key={project.id}
-            sx={{ width: 1 }}
-            variant="UserDashboardProjectSuggest.background"
-          >
-            <Stack
-              direction={{ sm: "column", md: "row" }}
-              spacing={{ sm: 1, md: 2 }}
-              justifyContent="space-between"
-              alignItems="center"
-              p={2}
-              sx={{
-                borderLeft: 4,
-                borderColor: "UserDashboardProjectSuggest.border",
-              }}
+          <Link href={`/project/${project.id}`} underline="none">
+            <Paper
+              key={project.id}
+              sx={{ width: "100%" }}
+              variant="UserDashboardProjectSuggest.background"
             >
-              <Stack
-                direction="column"
-                justifyContent="flex-start"
-                alignItems="flex-start"
-                spacing={0.5}
-              >
+              <Box sx={{ width: 500 }}>
                 <Stack
                   direction={{ sm: "column", md: "row" }}
                   spacing={{ sm: 1, md: 2 }}
                   justifyContent="space-between"
                   alignItems="center"
+                  p={2}
                   sx={{
-                    width: "100%",
+                    borderLeft: 4,
+                    borderColor: "UserDashboardProjectSuggest.border",
                   }}
                 >
                   <Stack
-                    direction={{ sm: "column", md: "column", width: 1 }}
-                    p={0}
+                    direction="column"
+                    justifyContent="flex-start"
+                    alignItems="flex-start"
+                    spacing={0.5}
                   >
-                    <Typography component="div" variant="h2">
-                      {project.project_name}
-                    </Typography>
-                    <Typography
-                      component="div"
-                      variant="UserDaskboardSuggestDate"
+                    <Stack
+                      direction={{ sm: "column", md: "row" }}
+                      spacing={{ sm: 1, md: 2 }}
+                      justifyContent="space-between"
+                      alignItems="center"
+                      sx={{
+                        width: 1,
+                      }}
                     >
-                      Du {formatDate(project.project_start_date)} au{" "}
-                      {formatDate(project.project_end_date)}
+                      <Stack
+                        direction={{ sm: "column", md: "column", width: 1 }}
+                        p={0}
+                      >
+                        <Typography component="div" variant="h2">
+                          {project.project_name}
+                        </Typography>
+                        <Typography
+                          component="div"
+                          variant="UserDaskboardSuggestDate"
+                        >
+                          Du {formatDate(project.project_start_date)} au{" "}
+                          {formatDate(project.project_end_date)}
+                        </Typography>
+                      </Stack>
+                      <Avatar
+                        alt="Remy Sharp"
+                        src="https://xsgames.co/randomusers/avatar.php?g=male"
+                      />
+                    </Stack>
+                    <Typography
+                      variant="body1"
+                      gutterBottom
+                      sx={{
+                        width: 460,
+                      }}
+                    >
+                      {project.project_description}
                     </Typography>
-                  </Stack>
-                  <Avatar
-                    alt="Remy Sharp"
-                    src="https://xsgames.co/randomusers/avatar.php?g=male"
-                  />
-                </Stack>
-                <Typography variant="body1" gutterBottom>
-                  {project.project_description}
-                </Typography>
 
-                <Stack
-                  direction="row"
-                  justifyContent="center"
-                  alignItems="flex-start"
-                  spacing={1}
-                  pt={2}
-                >
-                  <Chip label="Javascript" size="small" />
-                  <Chip label="Node.js" size="small" />
-                  <Chip label="React" size="small" />
+                    <Stack
+                      direction="row"
+                      justifyContent="center"
+                      alignItems="flex-start"
+                      spacing={1}
+                      pt={2}
+                    >
+                      {projectSkillListing
+                        .filter(
+                          (projectSkill) =>
+                            projectSkill.project_id === project.id
+                        )
+                        .map((projectSkill) => {
+                          const skillFilterd = skillListing.find(
+                            (skill) => skill.id === projectSkill.skill_id
+                          );
+                          return (
+                            <Chip
+                              key={skillFilterd.id}
+                              label={skillFilterd.skill_name}
+                              size="small"
+                            />
+                          );
+                        })}
+                    </Stack>
+                  </Stack>
                 </Stack>
-              </Stack>
-            </Stack>
-          </Paper>
+              </Box>
+            </Paper>
+          </Link>
         );
       })}
     </>
