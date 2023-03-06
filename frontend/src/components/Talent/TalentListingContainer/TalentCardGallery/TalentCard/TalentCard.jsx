feefbc415a40d3e11484b960bae65598b5fb2cbd
HoiAn2010 <Alinesonza@aol.com>
2023-03-06 11:57:07 +0100

    link to page singleTalent

--- frontend/src/components/Talent/TalentListingContainer/TalentCardGallery/TalentCard/TalentCard.jsx
+++ frontend/src/components/Talent/TalentListingContainer/TalentCardGallery/TalentCard/TalentCard.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { CardActionArea } from "@mui/material";
+import Link from "@mui/material/Link";
 import Stack from "@mui/material/Stack";
 import Paper from "@mui/material/Paper";
 import PropTypes from "prop-types";
@@ -8,40 +9,44 @@ import TalentCardName from "./TalentCardName/TalentCardName";
 import TalentCardDescription from "./TalentCardDescription/TalentCardDescription";
 
 export default function TalentCard({
+  id,
   firstname,
   lastname,
   jobName,
   biography,
 }) {
   return (
-    <Paper>
-      <CardActionArea>
-        <Stack
-          alignItems="center"
-          sx={{ p: 2, sm: "50%", md: "25%" }}
-          direction="column"
-        >
+    <Link href={`/talent/${id}`} underline="none">
+      <Paper>
+        <CardActionArea>
           <Stack
-            direction={{ xs: "row", sm: "row", md: "column" }}
-            justifyContent="center"
             alignItems="center"
-            spacing={2}
+            sx={{ p: 2, sm: "50%", md: "25%" }}
+            direction="column"
           >
-            <TalentAvatar />
-            <TalentCardName
-              jobName={jobName}
-              firstname={firstname}
-              lastname={lastname}
-            />
-          </Stack>
+            <Stack
+              direction={{ xs: "row", sm: "row", md: "column" }}
+              justifyContent="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <TalentAvatar />
+              <TalentCardName
+                jobName={jobName}
+                firstname={firstname}
+                lastname={lastname}
+              />
+            </Stack>
 
-          <TalentCardDescription biography={biography} />
-        </Stack>
-      </CardActionArea>
-    </Paper>
+            <TalentCardDescription biography={biography} />
+          </Stack>
+        </CardActionArea>
+      </Paper>
+    </Link>
   );
 }
 TalentCard.propTypes = {
+  id: PropTypes.number,
   jobName: PropTypes.string,
   firstname: PropTypes.string,
   lastname: PropTypes.string,
@@ -50,6 +55,7 @@ TalentCard.propTypes = {
 
 TalentCard.defaultProps = {
   jobName: "",
+  id: 0,
   firstname: "",
   lastname: "",
   biography: "",
