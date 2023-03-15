d6eece525549a7487948f2745d208df62b155787
fredl2tp <fleschira@gmail.com>
2023-03-15 03:52:36 +0100

    project form static header

--- frontend/src/components/Project/ProjectForm/ProjectFormHeading/ProjectFormHeading.jsx
+++ frontend/src/components/Project/ProjectForm/ProjectFormHeading/ProjectFormHeading.jsx
@@ -1,29 +1,26 @@
-// import React from "react";
-// import Stack from "@mui/material/Stack";
-// import Typography from "@mui/material/Typography";
-// import Box from "@mui/material/Box";
-// import Paper from "@mui/material/Paper";
-
-// export default function ProjectFormHeading() {
-//   return (
-//     <Paper elevation={8} sx={{ mb: 2 }}>
-//       <Box sx={{ width: "100%", background: "#FFF", padding: 3 }}>
-//         <Stack spacing={2}>
-//           <Typography variant="h1">Add Project</Typography>
-//           <Typography variant="h5">Neuralink</Typography>
-//           <Typography variant="subtitle1" gutterBottom>
-//             Projet de développement de la connectivité cerveau-machine.
-//           </Typography>
-//         </Stack>
-//       </Box>
-//     </Paper>
-//   );
-// }
-
 import React from "react";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 
-function ProjectFormHeading() {
-  return <div>ProjectFormBody</div>;
+export default function ProjectFormHeading() {
+  return (
+    <Paper elevation={8} sx={{ mb: 2 }}>
+      <Stack direction="row" spacing={2}>
+        <Box sx={{ width: "70%", background: "#FFF", padding: 3 }}>
+          <Stack spacing={2}>
+            <Typography variant="h1">Add Project</Typography>
+            <Typography variant="h5">Neuralink</Typography>
+            <Typography variant="subtitle1" gutterBottom>
+              Projet de développement de la connectivité cerveau-machine.
+            </Typography>
+          </Stack>
+        </Box>
+        <Box sx={{ width: "30%", background: "#FFF", padding: 3 }}>
+          <Typography variant="h1">Add Project</Typography>
+        </Box>
+      </Stack>
+    </Paper>
+  );
 }
-
-export default ProjectFormHeading;
