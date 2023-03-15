67c4941a19d193f61abd83f4c7bd470d973e7093
fredl2tp <fleschira@gmail.com>
2023-03-15 11:57:23 +0100

    form left right statis

--- frontend/src/components/Project/ProjectForm/ProjectFormBody/ProjectFormBody.jsx
+++ frontend/src/components/Project/ProjectForm/ProjectFormBody/ProjectFormBody.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
 import ProjectFormLeft from "./ProjectFormLeft/ProjectFormLeft";
-// import ProjectFormRight from "./ProjectFormRight/ProjectFormRight";
+import ProjectFormRight from "./ProjectFormRight/ProjectFormRight";
 
 export default function ProjectFormBody() {
   return (
-    <Stack spacing={2}>
+    <Stack direction="row" justifyContent="space-around" spacing={2}>
       <ProjectFormLeft />
-      {/* <ProjectFormRight /> */}
+      <ProjectFormRight />
     </Stack>
   );
 }
---
+++ frontend/src/components/Project/ProjectForm/ProjectFormBody/ProjectFormRight/ProjectFormRight.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectFormRight() {
+  return <div>ProjectFormRight</div>;
+}
+
+export default ProjectFormRight;
