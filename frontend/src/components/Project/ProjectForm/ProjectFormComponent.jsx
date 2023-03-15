d14e11b4cad4e89e524ab2f540ac87817eb282ae
fredl2tp <fleschira@gmail.com>
2023-03-15 10:57:59 +0100

    ProjectFormLeft ok

--- frontend/src/components/Project/ProjectForm/ProjectFormComponent.jsx
+++ frontend/src/components/Project/ProjectForm/ProjectFormComponent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Stack from "@mui/material/Stack";
 import ProjectFormHeading from "./ProjectFormHeading/ProjectFormHeading";
+import ProjectFormBody from "./ProjectFormBody/ProjectFormBody";
 
 export default function ProjectFormComponent() {
   return (
     <Stack spacing={2}>
       <ProjectFormHeading />
+      <ProjectFormBody />
     </Stack>
   );
 }
