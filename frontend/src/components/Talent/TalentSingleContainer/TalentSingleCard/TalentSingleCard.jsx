a52ce2e96037e6993a639afd8c3f7fc79b796bff
fredericgalline <frederic@galline.fr>
2023-03-16 14:18:01 +0100

    Merge branch 'dev' of into fix/my-setting-skills-to-api

---
+++ frontend/src/components/Talent/TalentSingleContainer/TalentSingleCard/TalentSingleCard.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import PropTypes from "prop-types";
+
+function TalentSingleCard({ avatar, firstName, lastName, bio }) {
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Paper
+        elevation={2}
+        sx={{
+          color: "UserDashboardCard.color",
+          backgroundColor: "UserDashboardCard.Background",
+          "&:hover": {
+            backgroundColor: "UserDashboardCard.Bghover",
+          },
+        }}
+      >
+        <Stack
+          direction={{ sm: "column", md: "row" }}
+          spacing={{ sm: 1, md: 2 }}
+          justifyContent="flex-start"
+          alignItems="center"
+          p={2}
+        >
+          <Avatar
+            alt="Remy Sharp"
+            src={avatar}
+            sx={{
+              width: 100,
+              height: 100,
+              border: 4,
+              borderColor: "primary.main",
+            }}
+          />
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+            spacing={0.5}
+            sx={{ width: "100%" }}
+          >
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              alignItems="flex-start"
+              spacing={0.5}
+            >
+              <Typography
+                component="div"
+                variant="h2"
+                sx={{
+                  color: "UserDashboardCard.color",
+                }}
+              >
+                {firstName} {lastName}
+              </Typography>
+            </Stack>
+            <Typography variant="body1" gutterBottom fullWidth>
+              {bio}
+            </Typography>
+          </Stack>
+        </Stack>
+      </Paper>
+    </Box>
+  );
+}
+
+TalentSingleCard.propTypes = {
+  avatar: PropTypes.string,
+  firstName: PropTypes.string,
+  lastName: PropTypes.string,
+  bio: PropTypes.string,
+};
+
+TalentSingleCard.defaultProps = {
+  avatar: "",
+  firstName: "",
+  lastName: "",
+  bio: "",
+};
+
+export default TalentSingleCard;
