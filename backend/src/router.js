bc2674646411c113f0b5779ed206f49f6edafad8
fredericgalline <frederic@galline.fr>
2023-03-21 16:36:37 +0100

    fix router

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const formidable = require("formidable");
 
 const router = express.Router();
 
@@ -7,15 +8,51 @@ const {
   verifyPassword,
   verifyToken,
   verifyId,
+  validateForm,
 } = require("./auth");
 
 // VERIFIE
-
 const userControllers = require("./controllers/userControllers");
+const regionControllers = require("./controllers/regionControllers");
+const jobControllers = require("./controllers/jobControllers");
+const skillControllers = require("./controllers/skillControllers");
+const userSkillControllers = require("./controllers/userSkillControllers");
+
+// ------ Upload -------
+
+router.post("/upload", (req, res) => {
+  const form = new formidable.IncomingForm(); // Assurez-vous que cette ligne est présente et correcte
+  form.uploadDir = "../../frontend/src/assets/projects-img/";
+  form.keepExtensions = true;
+
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.error("Error processing file upload:", err);
+      res.status(500).send("Error processing file upload.");
+      return;
+    }
+
+    // Process uploaded file(s) and/or fields here
+    const uploadedFile = files.project_image; // Remplacez "image" par le nom de l'input côté client
+    const filename = uploadedFile.name; // Utilisez .name pour obtenir le nom du fichier
+
+    res.status(200).json({ filename });
+  });
+});
+
+// ------ GET pour alimenter sign-up -------
+router.get("/regions", regionControllers.browse);
+router.get("/jobs", jobControllers.browse);
+router.get("/skills", skillControllers.browse);
+
 // -----REGISTER-------
 router.post("/users", hashPassword, userControllers.add);
+router.post("/user_skills", userSkillControllers.add);
+
+// -----LOGIN-------
 router.post(
   "/users/login",
+  validateForm,
   userControllers.getUserByEmailWithPasswordAndPassToNext,
   verifyPassword
 );
@@ -35,9 +72,8 @@ router.put("/user_roles/:id", userRoleControllers.edit);
 router.post("/user_roles", userRoleControllers.add);
 router.delete("/user_roles/:id", userRoleControllers.destroy);
 
-const jobControllers = require("./controllers/jobControllers");
+//  jobs
 
-router.get("/jobs", jobControllers.browse);
 router.get("/jobs/:id", jobControllers.read);
 router.put("/jobs/:id", jobControllers.edit);
 router.post("/jobs", jobControllers.add);
@@ -51,33 +87,37 @@ router.put("/candidacies/:id", candidacyControllers.edit);
 router.post("/candidacies", candidacyControllers.add);
 router.delete("/candidacies/:id", candidacyControllers.destroy);
 
-const userSkillControllers = require("./controllers/userSkillControllers");
-
 router.get("/user_skills", userSkillControllers.browse);
 router.get("/user_skills/:id", userSkillControllers.read);
 router.put("/user_skills/:id", userSkillControllers.edit);
-router.post("/user_skills", userSkillControllers.add);
 router.delete("/user_skills/:id", userSkillControllers.destroy);
 
 const projectControllers = require("./controllers/projectControllers");
 
+// projects
+
 router.get("/projects", projectControllers.browse);
 router.get("/projects/:id", projectControllers.read);
+router.get("/projects_current", projectControllers.filterProjectCurrent);
+router.get("/projects_upcoming", projectControllers.filterProjectUpComing);
+router.get(
+  "/projects_notselected",
+  projectControllers.filterProjectNotselected
+);
+router.get("/projects_all", projectControllers.filterAllProject);
 router.put("/projects/:id", projectControllers.edit);
 router.post("/projects", projectControllers.add);
 router.delete("/projects/:id", projectControllers.destroy);
 
-const skillControllers = require("./controllers/skillControllers");
+// skills
 
-router.get("/skills", skillControllers.browse);
 router.get("/skills/:id", skillControllers.read);
 router.put("/skills/:id", skillControllers.edit);
 router.post("/skills", skillControllers.add);
 router.delete("/skills/:id", skillControllers.destroy);
 
-const regionControllers = require("./controllers/regionControllers");
+// regions
 
-router.get("/regions", regionControllers.browse);
 router.get("/regions/:id", regionControllers.read);
 router.put("/regions/:id", regionControllers.edit);
 router.post("/regions", regionControllers.add);
