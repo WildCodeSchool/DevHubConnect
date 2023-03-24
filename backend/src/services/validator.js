f7058f9d581dde791de9b1ee5aeb0862ce288996
fredl2tp <fleschira@gmail.com>
2023-03-24 11:20:46 +0100

    eslint cleaning

--- backend/src/services/validator.js
+++ backend/src/services/validator.js
@@ -34,87 +34,67 @@ const validateLoginForm = (req, res, next) => {
   }
 };
 // -------------------------------------------------------------------------
-
+/* eslint-disable camelcase */
 const validateProjectForm = (req, res, next) => {
   // désactivation provisoire le camelcase de eslint pour traiter les tables mysql dans le middleware
 
   const {
-    // eslint-disable-next-line camelcase
     project_name,
-    // eslint-disable-next-line camelcase
     project_start_date,
-    // eslint-disable-next-line camelcase
     project_end_date,
-    // eslint-disable-next-line camelcase
     project_about,
-    // eslint-disable-next-line camelcase
     project_description,
-    // eslint-disable-next-line camelcase
     // project_state,
-    // eslint-disable-next-line camelcase
     // project_remote,
-    // eslint-disable-next-line camelcase
     // project_image,
-    // eslint-disable-next-line camelcase
     region_id,
-    // eslint-disable-next-line camelcase
     // creator_id,
-    // eslint-disable-next-line camelcase
     skillIds,
   } = req.body;
   const errors = [];
-  // eslint-disable-next-line camelcase
   if (!project_name?.length || project_name == null) {
     errors.push({
       field: "project_name",
       message: "Ce titre est obligatoire",
     });
-    // eslint-disable-next-line camelcase
   } else if (project_name.length >= 61) {
     errors.push({
       field: "project_name",
       message: "Ce champ doit contenir moins de 61 caractères",
     });
   }
-  // project_start_date-----------------------
-  // eslint-disable-next-line camelcase
+  // project_start_date----------------------
   if (!project_start_date?.length || project_start_date == null) {
-    // eslint-disable-next-line camelcase
     errors.push({
       field: "project_start_date",
       message: "Cette date est obligatoire",
     });
   }
-  // project_end_date-----------------------
-  // eslint-disable-next-line camelcase
+  // project_end_date----------------------
   if (!project_end_date?.length || project_end_date == null) {
     errors.push({
       field: "project_end_date",
       message: "Cette date est obligatoire",
     });
   }
-  // project_about-----------------------
-  // eslint-disable-next-line camelcase
+  // project_about----------------------
   if (!project_about?.length || project_about == null) {
     errors.push({
       field: "project_about",
       message: "Ces quelques mots sont obligatoires",
     });
-    // eslint-disable-next-line camelcase
   } else if (project_about.length >= 151) {
     errors.push({
       field: "project_about",
       message: "Ce champ doit contenir moins de 151 caractères",
     });
   }
-  // project_description-----------------------
-  // eslint-disable-next-line camelcase
+  // project_description----------------------
   if (!project_description?.length || project_description == null) {
     errors.push({
       field: "project_description",
       message: "Cette description est obligatoire",
     });
-    // eslint-disable-next-line camelcase
   } else if (project_description.length >= 2001) {
     errors.push({
       field: "project_description",
@@ -127,8 +107,7 @@ const validateProjectForm = (req, res, next) => {
 
   // project_image-----------------------
 
-  // region_id-----------------------
-  // eslint-disable-next-line camelcase
+  // region_id----------------------
   if (Number.isNaN(region_id) || region_id == null || !region_id?.length) {
     errors.push({
       field: "region_id",
